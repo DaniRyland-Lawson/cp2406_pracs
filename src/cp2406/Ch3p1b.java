3ee2bd19e762706bfc272e2f3aea21ad5ee5c3ba
DanielleLAWSON <danielle.lawson@my.jcu.edu.au>
2021-09-06 16:21:44 +1000

    pracs

--- src/cp2406/Ch3p1b.java
+++ src/cp2406/Ch3p1b.java
@@ -2,7 +2,7 @@ package cp2406;
 
 public class Ch3p1b {
     /*
-    How many times do you have to roll a pair of dice before they come up snake eyes?
+    How many times (on Average) do you have to roll a pair of dice before they come up snake eyes?
     You could do the experiment by rolling the dice by hand. Write a computer program that simulates the experiment.
      The program should report the number of rolls that it makes before the dice come up snake eyes.
      (Note: "Snake eyes" means that both dice show a value of 1.)
@@ -12,6 +12,18 @@ public class Ch3p1b {
     public static void main(String[] args) {
         rollOnce();
 
+        int num = 100000;
+        double count = 0;
+        for (int i = 0; i < num; i++) {
+            int[] res = rollOnce();
+            if (res[0] == 1 && res[1] == 1) {
+                count++;
+
+            }
+        }
+        System.out.println("prob = " + count/num);
+        double actual = (1./6) * (1./6);
+        System.out.println("actul=" + actual );;
     }
 
     public static int[] rollOnce() {
@@ -24,9 +36,9 @@ public class Ch3p1b {
         die2 = (int)(Math.random()*6) + 1;
         roll = die1 + die2;
 
-        System.out.println("The first die comes up " + die1);
-        System.out.println("The second die comes up " + die2);
-        System.out.println("Your total roll is " + roll);
+ //       System.out.println("The first die comes up " + die1);
+//        System.out.println("The second die comes up " + die2);
+//        System.out.println("Your total roll is " + roll);
 
         int[] ret = {die1, die2};
         return ret;
