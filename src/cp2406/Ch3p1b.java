3a0bffdc568f179f836938e31da20b0140401770
DanielleLAWSON <danielle.lawson@my.jcu.edu.au>
2021-09-06 12:31:16 +1000

    pracs

---
+++ src/cp2406/Ch3p1b.java
@@ -0,0 +1,38 @@
+package cp2406;
+
+public class Ch3p1b {
+    /*
+    How many times do you have to roll a pair of dice before they come up snake eyes?
+    You could do the experiment by rolling the dice by hand. Write a computer program that simulates the experiment.
+     The program should report the number of rolls that it makes before the dice come up snake eyes.
+     (Note: "Snake eyes" means that both dice show a value of 1.)
+     Exercise 2.2 explained how to simulate rolling a pair of dice.
+     */
+
+    public static void main(String[] args) {
+        rollOnce();
+
+    }
+
+    public static int[] rollOnce() {
+
+        int die1;   // The number on the first die.
+        int die2;   // The number on the second die.
+        int roll;   // The total roll (sum of the two dice).
+
+        die1 = (int)(Math.random()*6) + 1;
+        die2 = (int)(Math.random()*6) + 1;
+        roll = die1 + die2;
+
+        System.out.println("The first die comes up " + die1);
+        System.out.println("The second die comes up " + die2);
+        System.out.println("Your total roll is " + roll);
+
+        int[] ret = {die1, die2};
+        return ret;
+
+    }  // end main()
+
+}  // end class
+
+
